535c85081b97d541c0f1971feba9b6938b3e93d0
stephen-isaiah <stephen.dennis139@gmail.com>
2021-03-03 09:57:18 -0700

    Removed Container class and added html to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -85,7 +85,7 @@ contactForm.addEventListener('submit', (e)=>{
     }
   
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://strawhatcreatives.netlify.app/ ');
+    xhr.open('POST', 'https://strawhatcreatives.netlify.app/');
     xhr.setRequestHeader('content-type', 'application/json');
     xhr.onload = function(){
       console.log(xhr.responseText);
